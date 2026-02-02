0215eb992c19ff1c044522da4f75c45254925a02
Lathan Hay <lathan1623@me.com>
2026-02-02 14:03:25 -0600

    use my fork of colorscheme

---
+++ init.lua
@@ -0,0 +1,68 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.signcolumn = "yes"
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>p', '"+d<CR>')
+
+vim.pack.add({
+	{ src = "https://github.com/lathan1623/mini.hues" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/echasnovski/mini.pairs" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "<C-n>" : "<Tab>"', {
+	expr = true, noremap = true, silent = true
+})
+vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "<C-p>" : "<S-Tab>"', {
+	expr = true, noremap = true, silent = true
+})
+
+require "mason".setup()
+require "mini.pairs".setup()
+require('mini.pick').setup({
+	mappings = {
+		toggle_info = '<C-n>',
+		toggle_preview = '<C-p>',
+		move_down = '<Tab>',
+		move_up = '<S-Tab>',
+	},
+})
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>pf', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+
+vim.lsp.enable({ "lua_ls", "jdtls", "rust_analyzer", "ts_ls" })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+
+vim.o.termguicolors = true
+vim.cmd("colorscheme minispring")
+vim.cmd(":hi statusline guibg=NONE")
+vim.cmd [[
+  hi MiniPickCurrent guibg=#3E4452 guifg=#ABB2BF
+  hi MiniPickSelected guibg=#61AFEF guifg=#282C34
+]]
